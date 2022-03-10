ea901a6ade3820e852ffefec74aa462e41c81d39
RobertZapata <101352118+RobertZapata@users.noreply.github.com>
2022-03-10 18:21:56 -0500

    feat header component mispuntos

--- css-maquetacion/src/app/app-routing.module.ts
+++ css-maquetacion/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PuntosComponent } from './components/puntos/puntos.component';
+import { UberComponent } from './components/uber/uber.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'puntos',
+    component: PuntosComponent,
+  },
+  {
+    path: 'uber',
+    component: UberComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- css-maquetacion/src/app/app.module.ts
+++ css-maquetacion/src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PuntosComponent } from './components/puntos/puntos.component';
+import { UberComponent } from './components/uber/uber.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, NavbarComponent, PuntosComponent, UberComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
